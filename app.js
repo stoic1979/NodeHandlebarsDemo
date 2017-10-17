e60cdd71c5321290c6c69360555a2d5a1324638d
Navjot Singh <weavebytes@gmail.com>
2017-10-17 13:29:04 +0530

    some template tests

--- app.js
+++ app.js
@@ -18,19 +18,23 @@ app.set('view engine', 'handlebars');
 //-----------------------------------------------------
 
 app.get('/', function (req, res) {
-   	res.render("index", {'page_content' : '<br> welcome to our shpiing site <br>'});
+   	res.render("index", {'page_content' : '<br> welcome to our shpiing site <br>', 'published': true});
 })
 
 app.get('/clothes', function (req, res) {
-   	res.render("index", {'page_content' : '<br> Pant, Shirt, Cap etc... <br>'});
+   	res.render("index", {
+   		'page_content' : '<br> 50% sale on all clothes <br>',
+   		 'published': true,
+   		 'data_items': ['Jean', 'Shirt', 'Pant'],
+   		});
 })
 
 app.get('/electronics', function (req, res) {
-   	res.render("index", {'page_content' : '<br> mobile, tv, computer <br>'});
+   	res.render("index", {'page_content' : '<br> mobile, tv, computer <br>', 'published': true});
 })
 
 app.get('/stationary', function (req, res) {
-   	res.render("index", {'page_content' : '<br> notebook, pen, pencil <br>'});
+   	res.render("index", {'page_content' : '<br> notebook, pen, pencil <br>', 'published': true});
 })
 //------------------------------------------------
 // starting server
