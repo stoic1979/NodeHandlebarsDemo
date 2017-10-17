32b7e2d57b70ac6520a653952b43b263052d5412
Navjot Singh <weavebytes@gmail.com>
2017-10-17 12:49:07 +0530

    quick hbars demo

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var exphbs = require('express-handlebars');
+
+
+//------------------------------------------------
+// initializing app
+//------------------------------------------------
+
+app.set('views', path.join(__dirname, 'views') );
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//-----------------------------------------------------
+//   APP ROUTES
+//-----------------------------------------------------
+
+app.get('/', function (req, res) {
+   	res.render("index", {'page_content' : '<br> welcome to our shpiing site <br>'});
+})
+
+app.get('/clothes', function (req, res) {
+   	res.render("index", {'page_content' : '<br> Pant, Shirt, Cap etc... <br>'});
+})
+
+app.get('/electronics', function (req, res) {
+   	res.render("index", {'page_content' : '<br> mobile, tv, computer <br>'});
+})
+
+app.get('/stationary', function (req, res) {
+   	res.render("index", {'page_content' : '<br> notebook, pen, pencil <br>'});
+})
+//------------------------------------------------
+// starting server
+//------------------------------------------------
+var port = 3000;
+app.listen(port, function () {
+	console.log('Server started at port: ' + port );
+});
